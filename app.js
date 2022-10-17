495f0d75836d6007191d11e5d0a9a5e0e4dc9b1e
Goody <101070150+Goody4444@users.noreply.github.com>
2022-10-17 08:05:17 +0100

    fix commit

---
+++ app.js
@@ -0,0 +1,67 @@
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+
+hamburger.addEventListener('click', ()=>{
+   //Links
+    navLinks.classList.toggle("open");
+    links.forEach(link => {
+        link.classList.toggle("fade");
+    });
+
+    //Animation
+    hamburger.classList.toggle("toggle");
+});
+
+
+
+  {
+    
+
+  const slides = document.querySelectorAll(".slide");
+  const nextButton = document.getElementById("next");
+  const prevButton = document.getElementById("prev");
+  const auto = true;
+  const intervalTime = 5000;
+  let slideInterval;
+  
+  const nextSlide = () => {
+    const current = document.querySelector(".current");
+    current.classList.remove("current");
+    if (current.nextElementSibling) {
+      current.nextElementSibling.classList.add("current");
+    } else {
+      slides[0].classList.add("current");
+    }
+  };
+  
+  const prevSlide = () => {
+    const current = document.querySelector(".current");
+    current.classList.remove("current");
+    if (current.previousElementSibling) {
+      current.previousElementSibling.classList.add("current");
+    } else {
+      slides[slides.length - 1].classList.add("current");
+    }
+  };
+  
+  nextButton.addEventListener("click", () => {
+    nextSlide();
+    if (auto) {
+      clearInterval(slideInterval);
+      slideInterval = setInterval(nextSlide, intervalTime);
+    }
+  });
+  prevButton.addEventListener("click", () => {
+    prevSlide();
+    if (auto) {
+      clearInterval(slideInterval);
+      slideInterval = setInterval(nextSlide, intervalTime);
+    }
+  });
+  
+  if (auto) {
+    slideInterval = setInterval(nextSlide, intervalTime);
+  }
+   
+  }
